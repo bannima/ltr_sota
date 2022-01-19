9fa11a43f379e9d67da7e65ff3e7a50540e0f1d3
barryzhou <zhouenguo@126.com>
2022-01-19 19:58:54 +0800

    update wide&deep

--- ltr/models/wide_deep.py
+++ ltr/models/wide_deep.py
@@ -13,7 +13,7 @@ import torch
 import torch.nn as nn
 import numpy as np
 from ltr.modules.data_utils import get_activation
-from ltr.models.layers.core import dense_layer
+from ltr.models.layers.core import dense_layer,EmbAndConcat,MLP
 
 class Wide(nn.Module):
     def __init__(self, wide_dim: int, pred_dim: int = 1):
@@ -40,72 +40,6 @@ class Wide(nn.Module):
         out = self.wide_linear(inputs.long()).sum(dim=1) + self.bias
         return out
 
-class EmbAndConcat(nn.Module):
-    ''' category embedding layer and concatenate layer for deep model'''
-    def __init__(self,deep_column_idx, deep_continuous_cols, deep_emb_inputs,dropout=0.3):
-        super(EmbAndConcat, self).__init__()
-        self.deep_column_idx = deep_column_idx
-        self.deep_continuous_cols = deep_continuous_cols
-        self.deep_emb_inputs = deep_emb_inputs
-        #init category features embedding
-        self.embed_layers = nn.ModuleDict(
-            {
-                "emb_layer_{}".format(col):nn.Embedding(val+1,dim,padding_idx=0)
-                for col,val,dim in self.deep_emb_inputs
-            }
-        )
-        self.embedding_dropout = nn.Dropout(dropout)
-        self.emb_out_dim = np.sum([embed[2] for embed in self.deep_emb_inputs])
-
-        #init continuous features
-        self.cont_out_dim = len(deep_continuous_cols)
-
-        self.out_dim = self.emb_out_dim +self.cont_out_dim
-
-    def forward(self,x):
-        ''' given deep input x, return the concat of category embedding layer '''
-        embed_outs = [
-            self.embed_layers['emb_layer_{}'.format(col)](x[:,self.deep_column_idx[col]].long())
-            for col,val,dim in self.deep_emb_inputs
-        ]
-        #concat by batch size dim
-        embed_outs = torch.cat(embed_outs,dim=1)
-        embed_outs = self.embedding_dropout(embed_outs)
-
-        #continuous featrues
-        cont_outs = x[:,[self.deep_column_idx[col] for col in self.deep_continuous_cols]]
-
-        return embed_outs,cont_outs
-
-class MLP(nn.Module):
-    ''' basic deep model '''
-    def __init__(self, hidden_dims, dropouts,deep_column_idx,deep_continuous_cols, deep_emb_inputs):
-        super(MLP,self).__init__()
-        assert isinstance(hidden_dims, list)
-
-        #category features embedding and concat
-        self.emb_cat = EmbAndConcat(deep_column_idx,deep_continuous_cols, deep_emb_inputs)
-        #features should embedding first
-        self.hidden_dims = [self.emb_cat.out_dim]+hidden_dims
-        self.mlp = nn.Sequential()
-        for i in range(1,len(self.hidden_dims)):
-            self.mlp.add_module(
-                "dense_layer_{}".format(i-1),
-                dense_layer(self.hidden_dims[i-1],
-                            self.hidden_dims[i],
-                            activation='relu',
-                            dropout=dropouts[i-1])
-            )
-        for name,tensor in self.mlp.named_parameters():
-            if 'weight' in name:
-                nn.init.normal_(tensor,mean=0,std=0.0001)
-
-    def forward(self,x):
-        #transform to category embed and continuous out
-        embed_outs,cont_outs = self.emb_cat(x)
-        outs = torch.cat([embed_outs, cont_outs], 1).to(torch.float32)
-        return self.mlp(outs)
-
 class WideDeep(nn.Module):
     ''' Wide & Deep Networks '''
     def __init__(self,
@@ -119,16 +53,20 @@ class WideDeep(nn.Module):
                  ):
         super(WideDeep, self).__init__()
         self.wide = Wide(wide_dim=wide_dim,pred_dim=pred_dim)
-        self.deep = MLP(hidden_dims=deep_hidden_dims,
-                        dropouts=deep_dropouts,
-                        deep_column_idx = deep_column_idx,
-                        deep_continuous_cols =deep_continuous_cols,
-                        deep_emb_inputs = deep_emb_inputs
-                        )
+
+        # category features embedding and concat
+        self.emb_cat = EmbAndConcat(deep_column_idx, deep_continuous_cols, deep_emb_inputs)
+        self.deep = MLP(hidden_dims=[self.emb_cat.out_dim]+deep_hidden_dims,
+                        dropouts=deep_dropouts)
 
     def forward(self, inputs):
         wide_inputs = inputs['wide']
         deep_inputs = inputs['deep']
         wide_outs = self.wide(wide_inputs)
+
+        # transform to category embed and continuous out
+        embed_outs, cont_outs = self.emb_cat(deep_inputs)
+        deep_inputs = torch.cat([embed_outs, cont_outs], 1).to(torch.float32)
+
         wide_outs.add_(self.deep(deep_inputs))
         return wide_outs

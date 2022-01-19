2e7996e6d09be4df7400008ab6829cbe9f1cf084
barryzhou <zhouenguo@126.com>
2022-01-19 17:34:54 +0800

    update factorization machines

--- ltr/models/layers/interaction.py
+++ ltr/models/layers/interaction.py
@@ -10,7 +10,6 @@
 """
 import torch
 import torch.nn as nn
-from ltr.models.layers.core import dense_layer
 
 class FMLayer(nn.Module):
     """Factorization Machines
@@ -43,10 +42,25 @@ class FMLayer(nn.Module):
         :param x: 2D tensor with shape: ``(batch_size,feature_size)``
         :return: 2D tensor with shape: ``(batch_size,1)``
         '''
+
+        # linear_outs = self.linear(x) # B x 1
+        # v_v = torch.mm(self.V,self.V.T) # (N x K) @ (K x N) = N x N
+        # x = x.unsqueeze(dim=1) # B x 1 x N
+        # x_x = torch.bmm(x.transpose(-1,-2),x) # (B x N x 1) @ (B x 1 x N) = B x N x N
+        # interaction_outs = torch.matmul(x_x,v_v) # N x N, element-wise product
+        # linear_outs.add_(torch.sum(interaction_outs,dim=(1,2),keepdim=False).unsqueeze(dim=1)) # (B x N x N) -> (B x 1 x 1) -> B -> (B x 1)
+        # return linear_outs
+
+        batch_size = x.shape[0]
         linear_outs = self.linear(x) # B x 1
-        v_v = torch.mm(self.V,self.V.T) # (N x K) @ (K x N) = N x N
-        x = x.unsqueeze(dim=1) # B x 1 x N
-        x_x = torch.bmm(x.transpose(-1,-2),x) # (B x N x 1) @ (B x 1 x N) = B x N x N
-        interaction_outs = torch.matmul(x_x,v_v) # N x N, element-wise product
-        linear_outs.add_(torch.sum(interaction_outs,dim=(1,2),keepdim=False).unsqueeze(dim=1)) # (B x N x N) -> (B x 1 x 1) -> B -> (B x 1)
+
+        sum_of_square = torch.pow(torch.matmul(x.unsqueeze(dim=1),self.V).squeeze(dim=1),2)
+        sum_of_square = 0.5*torch.sum(sum_of_square,dim=1,keepdim=True)
+
+        square_of_sum = torch.matmul(x.unsqueeze(dim=2).expand(batch_size,self.n_dim,self.n_dim),self.V)
+        square_of_sum = torch.bmm(square_of_sum,square_of_sum.transpose(-1,-2))
+        square_of_sum = -0.5 * torch.sum(square_of_sum,dim=(1,2),keepdim=True).squeeze(dim=2)
+
+        linear_outs.add_(sum_of_square)
+        linear_outs.add_(square_of_sum)
         return linear_outs
